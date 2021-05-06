67f65252e6b71ab55b90a7fce6c51d06a66de80a
Amisha Tiwari <amisha26>
2021-05-06 12:59:10 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbars from './components/Navbars';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navbars />
+      </div>
+    );
+  }
+}
+
+export default App;
